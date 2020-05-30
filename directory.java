af1ccaadb30fe602b4001ae822c68c4c9a96a2e4
Akash Kumar Singh <55013687+akashrajput25@users.noreply.github.com>
2020-05-30 23:12:53 +0530

    Update directory.java

--- directory.java
+++ directory.java
@@ -12,10 +12,10 @@ public class directory
   {
 	public boolean accept(File pathname)
 	{
-	 return pathname.isDirectory();
+	 return pathname.isDirectory();                                       //returning the directory given by user
 	}
   });
-  System.out.println("\nDirectory of " +direct.getPath());
+  System.out.println("\nDirectory of " +direct.getPath());                    //printing current path of the file inputed by user
   listfile(direct);
   for(File folder:subdirs)
   {
@@ -27,21 +27,23 @@ public class directory
   File[] files =direct.listFiles();
   @SuppressWarnings("unused")
   String ans="";
-  java.util.Date myDate = new java.util.Date(direct.lastModified());
+  java.util.Date myDate = new java.util.Date(direct.lastModified());          //checking for last modified date of each file
   System.out.println("  name                                          length                  modified             code");
-	   String code = "";
-	   for(int i = 0; i < files.length; i++)
+	   
+	   for(int i = 0; i < files.length; i++)                              //iterating upto no of files in the current directory
 	   { 
-		  code="";
+		  String code = "";                                           //for every iteration the string code is initialised as an empty string
 	      if(files[i].canRead()&&files[i].canWrite())
-	          code += "rw";
+	          code +="rw";                                                //push rw to string code if file is both readable and writable
 	      else if(files[i].canRead()&&!(files[i].canWrite()))
-	    	  code+="r-";
+	    	  code+="r-";                                                 //push r- to string code if file is readable and not writable
 	      else if(!(files[i].canRead())&& files[i].canWrite())
-	    	  code+="w-";
+	    	  code+="w-";                                                 //push w- to string code if file is not readable and but writable
 	      else
-	    	  code+="--";
-           System.out.printf("%-46s  %-10d "+"  "+myDate+"    "+" %-1s ",files[i].getName(),files[i].length(),code);	
+	    	  code+="--";                                                 //push -- to string code if file is neither readable nor writable
+	      
+	       //printing all the file attributes such as :name ,length,last modified date,readable,writable,both or neither 	
+           System.out.printf("%-46s  %-10d "+"  "+myDate+"    "+" %-1s ",files[i].getName(),files[i].length(),code);
            System.out.println("\n");
        }
    }
@@ -49,27 +51,29 @@ public static void main(String args[]) throws IOException
  {
   String ans="";
   Scanner sc=new Scanner(System.in);
-  System.out.println("Hello ,Please enter your name\n");
+  System.out.println("Hello ,Please enter your name\n");                     //Ask the user for his/her name
   String name=sc.nextLine();
-  while(!ans.equals("N"))
+  name.toUpperCase(); 
+  while(!ans.equals("N"))                                                    //If answer equals "N" then terminate the while loop
   {
-   System.out.println("Hi "+name+" please enter the directory path\n");
-   String x="c:\\";
-   String direct=x+sc.nextLine();
+   System.out.println("Hi "+name+" please enter the directory path\n");      //ask the user for the directory name 
+   String x="c:\\cswendterm\\";                                              //my project's folder is currently in c:\cswendterm\directory
+   String z=sc.nextLine();
+   String direct=x+z;                                                        //String direct is the final path to look files in the directory
    if(new File(direct).exists())
    {
-    new directory().listfolder(new File(direct));
+    new directory().listfolder(new File(direct));                            //if the directory exists then show all the files and subfolders in it
    }	
    else
    {
-	  System.out.println("Sorry "+name+" directory not exist");
+	  System.out.println("Sorry "+name+" directory not exist");          //if directory doesn't exist then prompt the user that directory doesn't exist
    }
-   System.out.println("Do you want to look for another directory(Y/N) \n");
+   System.out.println("Do you want to look for another directory(Y/N) \n");  // check if the user wants to search another directory or not,If yes then process starts again from line 55.
    ans=sc.nextLine();
    ans.toUpperCase();
    if(ans.equals("N"))
    {  
-	  System.out.println("\nThanks for trying this UI "+name);
+	  System.out.println("\nThanks for trying this UI "+name);          //if user enters "N" then a thank you message for him/her
    }
   }
  }
